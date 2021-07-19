4261534e7d304f5253ece8c3b47ca1a0a3d683dc
Vadim Shpet <vadik.shpet01@gmail.com>
2021-07-19 14:01:28 +0300

    added fonts, images. Created .less and .js files

--- gulpfile.js
+++ gulpfile.js
@@ -10,55 +10,66 @@ const less = require('gulp-less');
 
 
 const styleFiles = [
+    './src/css/fontawesome.min.css',
+    './src/css/fonts.less',
     './src/css/main.less',
+    './src/css/sticky_header.less',
+    './src/css/header.less',
+    './src/css/specialities.less',
+    './src/css/menu.less',
+    './src/css/private_events.less'
 ]
 const jsFiles = [
-    './src/js/main.js'
+    './src/js/main.js',
+    './src/js/smooth_scroll.js',
+    './src/js/timer.js',
+    './src/js/active_table.js',
+    './src/js/slider.js'
 ]
 
 gulp.task('styles', () => {
     //шаблон для поиска файлов стилей
     return gulp.src(styleFiles)
-    .pipe(srcmaps.init())
-    .pipe(less()) 
-    //объеденение файлов в один
-    .pipe(concat('style.css'))
+        .pipe(srcmaps.init())
+        .pipe(less())
+        //объеденение файлов в один
+        .pipe(concat('style.css'))
 
-    .pipe(autoprefixer({
-        cascade: false
-    }))
-    .pipe(cleanCSS({
-        level: 2
-    }))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
 
-    .pipe(srcmaps.write('./'))
-    //выходная папка для стилей
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.stream());
+        .pipe(srcmaps.write('./'))
+        //выходная папка для стилей
+        .pipe(gulp.dest('./build/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', () => {
     return gulp.src(jsFiles)
 
-    .pipe(concat('script.js')) 
+        .pipe(concat('script.js'))
 
-    .pipe(uglify({
-        toplevel: true
-    }))
+        .pipe(uglify({
+            toplevel: true
+        }))
 
-    .pipe(gulp.dest('./build/js'))
-    .pipe(browserSync.stream());
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.stream());
 }
 );
 
 
-gulp.task('del', ()=> {
+gulp.task('del', () => {
     return del(['build/*'])
 });
 
-gulp.task('watch', ()=>{
+gulp.task('watch', () => {
     browserSync.init({
-        server:{
+        server: {
             baseDir: "./"
         }
     });
