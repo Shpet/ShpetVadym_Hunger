4a7d38cd432a41658facec567f2af6589c11d516
Vadim Shpet <vadik.shpet01@gmail.com>
2021-07-21 17:07:38 +0300

    added adaptive for 'spicialities'

--- src/js/active_table.js
+++ src/js/active_table.js
@@ -7,11 +7,18 @@ menuButts.forEach(item => {
             mainText = document.querySelectorAll('.menu__dishes .main-text'),
             subText = document.querySelectorAll('.menu__dishes .sub-text');
 
+
         for (let i = 0; i < menuLink.length; i++) {
             // было бы хорошо менять ссылки, но их нет
             menuLink[i].setAttribute('href', '#');
-            mainText[i].innerText = `${item.textContent} . . . . 32.3 USD`;
-            subText[i].innerText = "New subtext";
+            if (item.textContent === 'pizza') {
+                mainText[i].innerText = 'PIZZA QUATRO STAGIONI . . . . 55,68 USD';
+                subText[i].innerText = "Integer ullamcorper neque eu purus euismod";
+            }
+            else {
+                mainText[i].innerText = `${item.textContent} . . . . 32,3 USD`;
+                subText[i].innerText = "New subtext";
+            }
         }
     })
 })
